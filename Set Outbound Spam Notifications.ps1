f0121c129806de1c2cfe9bf3207318a90cc08929
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-09-06 17:32:26 -0600

    Add files via upload

---
+++ Set Outbound Spam Notifications.ps1
@@ -0,0 +1,13 @@
+﻿Function Connect-EXOnline {
+    $credentials = Get-Credential
+    Write-Output "Getting the Exchange Online cmdlets"
+    $session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+        -ConfigurationName Microsoft.Exchange -Credential $credentials `
+        -Authentication Basic -AllowRedirection
+    Import-PSSession $session
+}
+Connect-EXOnline
+
+$NotifcationEntity = Read-Host -Prompt "Enter the email of the recipient who will receive the spam notifications"
+
+Set-HostedOutboundSpamFilterPolicy Default -NotifyOutboundSpam $true -NotifyOutboundSpamRecipients $NotifcationEntity
