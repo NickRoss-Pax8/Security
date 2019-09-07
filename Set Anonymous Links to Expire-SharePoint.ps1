5d6e819d34d1d8fb8d924e4cd97c459abfcc7eae
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-09-07 11:55:34 -0600

    Add files via upload

---
+++ Set Anonymous Links to Expire-SharePoint.ps1
@@ -0,0 +1,10 @@
+﻿Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+$SharePointURL = Read-Host -Prompt "Enter your Tenants SharePoint Admin URL. This is typically in the format of https://domain-admin.sharepoint.com"
+
+Connect-SPOService -url $SharePointURL
+
+
+Set-SPOTenant -SharingCapability ExternalUserAndGuestSharing
+
+Set-SPOTenant -RequireAnonymousLinksExpireInDays 7 
