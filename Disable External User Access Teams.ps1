6daf705ba3a2fa0c16f1f140200562669adced57
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-09-14 11:43:29 -0600

    Add files via upload

---
+++ Disable External User Access Teams.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module SkypeOnlineConnector
+$Cred = Get-Credential
+$CSSession = New-CsOnlineSession -Credential $Cred
+Import-PSSession -Session $CSSession
+
+Set-CsTeamsClientConfiguration -AllowGuestUser $False
