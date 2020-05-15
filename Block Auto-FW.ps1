2054999b4dd4eb4b396715e22aef7202fffcf8f8
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2020-05-15 07:19:26 -0600

    Update Block Auto-FW.ps1

--- Block Auto-FW.ps1
+++ Block Auto-FW.ps1
@@ -1,19 +1,16 @@
-﻿Function Connect-EXOnline {
-    $credentials = Get-Credential
-    Write-Output "Getting the Exchange Online cmdlets"
-    $session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
-        -ConfigurationName Microsoft.Exchange -Credential $credentials `
-        -Authentication Basic -AllowRedirection
-    Import-PSSession $session
-}
-Connect-EXOnline
+Install-Module PowershellGet -Force
+Update-Module PowershellGet
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+
+Connect-ExchangeOnline
  
-$externalTransportRuleName = "Inbox Rules To External Block"
-$rejectMessageText = "To improve security, auto-forwarding rules to external addresses has been disabled. Please contact your Microsoft Partner if you'd like to set up an exception."
+$externalTransportRuleName = "Block Auto-Forwarding"
+$rejectMessageText = "To improve security, auto-forwarding rules to external email addresses have been disabled. Please contanct your helpdesk if you want to create an exception."
  
 $externalForwardRule = Get-TransportRule | Where-Object {$_.Identity -contains $externalTransportRuleName}
  
 if (!$externalForwardRule) {
-    Write-Output "Client Rules To External Block not found, creating Rule"
-    New-TransportRule -name "Client Rules To External Block" -Priority 1 -SentToScope NotInOrganization -FromScope InOrganization -MessageTypeMatches AutoForward -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText $rejectMessageText
+    Write-Output "Rule for Auto-forwarding not found, creating Rule"
+    New-TransportRule -name "Block Auto-forwarding" -Priority 1 -SentToScope NotInOrganization -FromScope InOrganization -MessageTypeMatches AutoForward -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText $rejectMessageText
 }   
