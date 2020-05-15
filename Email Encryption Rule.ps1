4237f074a4be86aba3b61849a1bd5b76916b8bb2
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2020-05-15 07:19:44 -0600

    Update Email Encryption Rule.ps1

--- Email Encryption Rule.ps1
+++ Email Encryption Rule.ps1
@@ -1,11 +1,8 @@
-Function Connect-EXOnline {
-    $credentials = Get-Credential
-    Write-Output "Getting the Exchange Online cmdlets"
-    $session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
-        -ConfigurationName Microsoft.Exchange -Credential $credentials `
-        -Authentication Basic -AllowRedirection
-    Import-PSSession $session
-}
-Connect-EXOnline
+Install-Module PowershellGet -Force
+Update-Module PowershellGet
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+
+Connect-ExchangeOnline
 
 New-TransportRule -Name "Encrypt Email" -SubjectContainsWords "Secure" -ApplyRightsProtectionTemplate "Encrypt"
