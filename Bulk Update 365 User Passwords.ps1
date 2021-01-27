9668ed140977e73a61877ea31856b92aed0e30f6
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2021-01-27 09:25:48 -0700

    Add files via upload

---
+++ Bulk Update 365 User Passwords.ps1
@@ -0,0 +1,21 @@
+﻿##########Connect to Exchange Online##########
+
+Write-Host -Prompt "Connecting to Exchange Online"
+
+$credential = Get-Credential
+
+Install-module Msonline
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+############# Define CSV path of Users and Group ##################
+
+$UserPath = Read-Host -Prompt "Enter File Path For CSV list of users"
+
+
+#####Create CSV template with headers of Userprincipalname and Password#######
+
+
+Import-Csv -Path $UserPath |%{Set-MsolUserPassword -userPrincipalName $_.UserPrincipalName –NewPassword $_.Password -ForceChangePassword $True}
